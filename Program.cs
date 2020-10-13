42f2cbf4a2923d3d838760eb0b1eeabe215b6237
ssmlnsk <ssmlnsk@mail.ru>
2020-10-13 23:30:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Marriage_Agency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
